4758118b366b4bb89a8f4a3b7ed215419e196bd9
MYiLA <polus180@ya.ru>
2020-04-15 01:10:56 +0300

    mobile css for blog created

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,11 @@ var rename = require("gulp-rename");
 var server = require("browser-sync").create();
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
-var csso = require("gulp-csso");
 var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore")
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
-var uglify = require("gulp-uglify");
-var htmlmin = require("gulp-htmlmin")
 var autoprefixer = require("autoprefixer");
 
 gulp.task("clean", function () {
@@ -23,8 +19,9 @@ gulp.task("clean", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
-      "source/fonts/*.{woff2}",
-      "source/img/**"
+      "source/fonts/*",
+      "source/img/**",
+      "source/lib/**"
     ], {
       base: "source"
     })
@@ -33,22 +30,20 @@ gulp.task("copy", function () {
 
 gulp.task("js", function () {
   return gulp.src("source/js/**")
-    .pipe(uglify())
     .pipe(gulp.dest("build/js"))
 })
 
 gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
+  return gulp.src("source/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
+    .pipe(rename("style.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("build"))
     .pipe(server.stream());
 });
 gulp.task("sprite", function () {
@@ -62,7 +57,6 @@ gulp.task("sprite", function () {
 gulp.task("html", function () {
   return gulp.src("source/*.html")
     .pipe(posthtml([include()]))
-    .pipe(htmlmin())
     .pipe(gulp.dest("build"));
 });
 gulp.task("images", function () {
@@ -76,14 +70,6 @@ gulp.task("images", function () {
       }),
       imagemin.svgo()
     ]))
-    .pipe(gulp.dest("source/img"));
-});
-
-gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({
-      quality: 90
-    }))
     .pipe(gulp.dest("source/img"));
 });
 
@@ -101,7 +87,7 @@ gulp.task("server", function () {
     done();
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
   gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
@@ -123,26 +109,4 @@ gulp.task("start", gulp.series(
 
 gulp.task("optimizeimg", gulp.series(
   "images",
-  "webp"
 ));
-
-  //   "browser-sync": "2.26.x",
-  //   "gulp": "^4.0.2",
-  //   "gulp-csso": "^3.0.1",
-  //   "gulp-imagemin": "^6.1.0",
-  //   "gulp-plumber": "1.2.x",
-  //   "gulp-postcss": "8.0.x",
-  //   "gulp-posthtml": "^3.0.4",
-  //   "gulp-rename": "^1.4.0",
-  //   "gulp-sass": "^4.0.2",
-  //   "gulp-sourcemaps": "2.6.x",
-  //   "gulp-svgstore": "^7.0.1",
-  //   "gulp-webp": "^4.0.1",
-  //   "posthtml-include": "^1.2.0",
-  //   "stylelint": "10.1.x"},
-  // "scripts": {
-  //   "stylelint": "stylelint \"source/sass/**/*.scss\" --syntax scss",
-  //   "test": "npm run stylelint",
-  //   "build": "gulp build",
-  //   "optimizeimg": "gulp optimizeimg",
-  //   "start": "gulp start"
