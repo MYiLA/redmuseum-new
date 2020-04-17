c212732f941daca92354c3d02809b5abb91981b3
MYiLA <polus180@ya.ru>
2020-04-17 08:11:26 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 // растворение .opacity--0
-
+(function () {
 const headerMenuElement = document.querySelector('.header__menu')
 //здесь можно разобраться с дребезжанием
 document.addEventListener('scroll', function () {
@@ -78,3 +78,51 @@ tagsLinkElements.forEach(element => {
     element.classList.add('active')
   })
 })
+
+// открытие попапов ссылок
+const notesNumArr = [... document.querySelectorAll('.article-page__note-num')];
+const notesPopupArr = [... document.querySelectorAll('.article-page__note-popup')];
+const notesPopupCloseArr = document.querySelectorAll('.article-page__note-close');
+
+// .article-page__note-num кнопки-ссылки
+// article-page__note-popup active попап активный
+// article-page__note-close внутри попапа кнопка закрытия
+// setTimeout(function () {
+//   tagsElement.classList.add('opacity--0');
+// }, 0);
+
+
+
+const closePopup = function(popup) {
+  setTimeout(function () {
+    popup.classList.add('opacity--0');
+  }, 0);
+  setTimeout(function () {
+    popup.classList.remove('active');
+  }, 600);
+}
+
+const openPopup = function (popup) {
+  popup.classList.add('active')
+  setTimeout(function () {
+    popup.classList.remove('opacity--0');
+  }, 0);
+}
+
+notesPopupCloseArr.forEach(element => {
+  element.addEventListener('click', function(evt) {
+    console.log(element.parentNode)
+    evt.stopPropagation();
+    closePopup(element.parentNode);
+  })
+})
+
+notesNumArr.forEach((element, i) => {
+  element.addEventListener('click', (evt) => {
+    openPopup(notesPopupArr[i]);
+  })
+})
+
+
+
+})();
