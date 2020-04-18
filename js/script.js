f18762814665387bdb6a15a5a72aa41131003d91
MYiLA <polus180@ya.ru>
2020-04-18 03:35:32 +0300

    refresh

--- js/script.js
+++ js/script.js
@@ -1,77 +1,58 @@
 'use strict';
-// растворение .opacity--0
 (function () {
-const headerMenuElement = document.querySelector('.header__menu')
-//здесь можно разобраться с дребезжанием
-document.addEventListener('scroll', function () {
-  if (document.documentElement.scrollTop <= 1) {
-    headerMenuElement.classList.remove('fixed');
-  } else {
-    headerMenuElement.classList.add('fixed');
-  }
-})
 
 // плавный скролл якорей
 
-// $(document).ready(function () {
-//   $('a').on('click', function (event) {
-//     if (this.hash !== "") {
-//       event.preventDefault();
-//       var hash = this.hash;
-
-//       $('html, body').animate({
-//         scrollTop: ($(hash).offset().top) - 100
-//       }, 800);
-//     }
-//   });
-// });
+var linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
+    V = 0.13;  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
+        e.preventDefault(); //отменяем стандартное поведение
+        var w = window.pageYOffset,  // производим прокрутка
+            hash = this.href.replace(/[^#]*(.*)/, '$1');  // к id элемента, к которому нужно перейти
+        var t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+            start = null;
+        requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+        function step(time) {
+            if (start === null) start = time;
+            var progress = time - start,
+                r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+            window.scrollTo(0,r);
+            if (r != w + t) {
+                requestAnimationFrame(step)
+            } else {
+                location.hash = hash  // URL с хэшем
+            }
+        }
+    }, false);
+}
 
 // При нажатии на "поиск и категории" 
 // появляется строка поиска и разворачивается облако категорий и тегов.
-// .tags.active
 
-const tagsElement = document.querySelector('.tags')
+const tagsElement = document.querySelector('.header__tags')
 const searchInputElement = document.querySelector('.search__input')
 const searchLabelElement = document.querySelector('.search__label')
 const tagsLinkElements = document.querySelectorAll('.tags__link');
 const searchResetElement = document.querySelector('.search__reset')
 
 searchInputElement.addEventListener('click', function () {
-  tagsElement.classList.add('active')
-  setTimeout(function () {
-    tagsElement.classList.remove('opacity--0');
-  }, 0);
+  tagsElement.classList.add('active');
+  searchResetElement.classList.add('active');
+  searchLabelElement.classList.add('opacity--0');
 })
-
-searchLabelElement.addEventListener('click', function () {
-  tagsElement.classList.add('active')
-  searchLabelElement.classList.add('opacity--0')
-  setTimeout(function () {
-    tagsElement.classList.remove('opacity--0');
-  }, 0);
-})
-
-
 
 searchResetElement.addEventListener('click', function () {
-  setTimeout(function () {
-    tagsElement.classList.add('opacity--0');
-  }, 0);
-
-  setTimeout(function () {
-    tagsElement.classList.remove('active')
+    tagsElement.classList.remove('active');
+    searchLabelElement.classList.remove('opacity--0');
+    searchResetElement.classList.remove('active');
     tagsLinkElements.forEach(element => {
-      element.addEventListener('click', function () {
-        element.classList.remove('active')
-      })
+      element.classList.remove('active')
     })
-  }, 400)
 })
 
-// Категории / теги : при нажатии на любое из значений — моментально фильтруется выдача 
-// (выводятся записи, содержащие выбранное значение). После нажатия на значение оно меняет цвет на черный. При повторном нажатии выбор отменяется. 
+// Категории / теги : После нажатия на значение оно меняет цвет на черный. При повторном нажатии выбор отменяется. 
 // Возможно одновременно выбрать сколько угодно значений.
-
 
 tagsLinkElements.forEach(element => {
   element.addEventListener('click', function () {
@@ -79,19 +60,11 @@ tagsLinkElements.forEach(element => {
   })
 })
 
-// открытие попапов ссылок
 const notesNumArr = [... document.querySelectorAll('.article-page__note-num')];
 const notesPopupArr = [... document.querySelectorAll('.article-page__note-popup')];
 const notesPopupCloseArr = document.querySelectorAll('.article-page__note-close');
 
-// .article-page__note-num кнопки-ссылки
-// article-page__note-popup active попап активный
-// article-page__note-close внутри попапа кнопка закрытия
-// setTimeout(function () {
-//   tagsElement.classList.add('opacity--0');
-// }, 0);
-
-
+// открытие попапов ссылок
 
 const closePopup = function(popup) {
   setTimeout(function () {
@@ -103,7 +76,7 @@ const closePopup = function(popup) {
 }
 
 const openPopup = function (popup) {
-  popup.classList.add('active')
+  popup.classList.add('active');
   setTimeout(function () {
     popup.classList.remove('opacity--0');
   }, 0);
@@ -111,7 +84,6 @@ const openPopup = function (popup) {
 
 notesPopupCloseArr.forEach(element => {
   element.addEventListener('click', function(evt) {
-    console.log(element.parentNode)
     evt.stopPropagation();
     closePopup(element.parentNode);
   })
@@ -123,6 +95,12 @@ notesNumArr.forEach((element, i) => {
   })
 })
 
+const sharingArrow = document.querySelector('.sharing__link-arrow');
 
+window.addEventListener('scroll', ()=>{
+  if(window.pageYOffset <= 1000) {
+    sharingArrow.classList.remove('active');
+  } else sharingArrow.classList.add('active');
+})
 
 })();
